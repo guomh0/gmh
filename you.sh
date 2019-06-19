f13911b006a612cc6a5a7c74b630bec552f048c8
guomh0 <guomh127@163.com>
2019-06-19 18:57:36 +0800

    yy sh

---
+++ you.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+read -p 'UesrName: ' username
+read -p 'PassWord: ' passward
+if [ ${username} == 'you' ];then
+ if [ ${password} == '123' ];
+  echo 'Load successfully'
+ fi
+fi
